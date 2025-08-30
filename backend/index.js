72dc8034a014f1c2922eb1617784e8ce936c756a
abhinav5568 <159443914+abhinav5568@users.noreply.github.com>
2025-08-30 12:32:08 +0530

    changed the base url to relative in front end, for acessing the backend

--- backend/index.js
+++ backend/index.js
@@ -8,6 +8,8 @@ morgan.token('body', (req, res) => JSON.stringify(req.body));
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 app.use(express.json());
 
+app.use(express.static('dist'));
+
 let persons = [
   {
     id: "1",
