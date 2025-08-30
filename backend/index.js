523aa691aeda522326794a20382605bb092c12c1
abhinav5568 <159443914+abhinav5568@users.noreply.github.com>
2025-08-30 12:39:47 +0530

    added vite config to redirect server requests (fixed the development build) removed the cors from backend, as its no longer needed
    fixed the

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,6 @@
 const express = require("express");
 const app = express();
 var morgan = require('morgan');
-const cors = require('cors');
-app.use(cors({ origin: "http://localhost:5173" }));
 
 morgan.token('body', (req, res) => JSON.stringify(req.body));
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
